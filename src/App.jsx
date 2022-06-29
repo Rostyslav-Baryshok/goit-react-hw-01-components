193d945cefd6546ae3e130bfe740cb593671aa4f
Rostyslav-Baryshok <85187900+Rostyslav-Baryshok@users.noreply.github.com>
2022-06-29 12:25:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import user from './data/user.json';
 import statistics from './data/statistics.json';
 import friends from './data/friends.json';
 import transactions from './data/transactions.json';
-import { Profile } from 'components/profile/Profile';
+import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
