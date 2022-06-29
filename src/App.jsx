c8c9786304ef6414867ebe0936cb56a2970303a3
Rostyslav-Baryshok <85187900+Rostyslav-Baryshok@users.noreply.github.com>
2022-06-29 12:37:12 +0300

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import user from './data/user.json';
 import statistics from './data/statistics.json';
 import friends from './data/friends.json';
 import transactions from './data/transactions.json';
-import { Profile } from 'components/Еrofile/Profile';
-import { Statistics } from 'components/Etatistics/Statistics';
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 
